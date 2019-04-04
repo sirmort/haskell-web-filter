c06061c3d6f69c36891a73a391830dd8fe1d82bf
FiveError <ser9800@mail.ru>
2019-04-04 18:06:02 +0300

    RGB filters

---
+++ src/ImageFilters.hs
@@ -0,0 +1,108 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE QuasiQuotes           #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleContexts #-}
+module ImageFilters where
+
+import Foundation
+import Yesod.Core
+import Vision.Image
+--import Data.Conduit
+import Debug.Trace
+
+import qualified Vision.Image.JuicyPixels   as Friday
+import qualified Vision.Primitive           as Friday
+import qualified Codec.Picture              as JP
+import qualified Codec.Picture.Types        as JP
+
+import qualified System.FilePath            as FilePath
+import qualified Data.Text                  as Text
+
+import Control.Monad.Trans.Resource (runResourceT)
+
+import Data.List
+import Data.Word8
+import Data.Bits
+
+toRed :: (Image i, Convertible i RGB) => i -> RGB
+toRed img =
+    let grey = convert img :: RGB
+    in fromFunction (shape img) $ \pt ->
+        let pix = grey `index` pt
+            r = fromIntegral $ rgbRed pix
+            g = fromIntegral $ rgbGreen pix
+            b = fromIntegral $ rgbBlue pix
+            tr = red r g b
+            tg = 0
+            tb = 0
+            pixel = RGBPixel { rgbRed = tr, rgbGreen = tg, rgbBlue = tb } in
+        pixel
+        where
+			red :: Int -> Int -> Int -> Word8
+			red r g b = intToWord8 $ round $ (1 * toRational r + 0 * toRational g + 0 * toRational b)
+			
+toGreen :: (Image i, Convertible i RGB) => i -> RGB
+toGreen img =
+    let grey = convert img :: RGB
+    in fromFunction (shape img) $ \pt ->
+        let pix = grey `index` pt
+            r = fromIntegral $ rgbRed pix
+            g = fromIntegral $ rgbGreen pix
+            b = fromIntegral $ rgbBlue pix
+            tr = 0
+            tg = green r g b
+            tb = 0
+            pixel = RGBPixel { rgbRed = tr, rgbGreen = tg, rgbBlue = tb } in
+        pixel
+        where
+			green :: Int -> Int -> Int -> Word8
+			green r g b = intToWord8 $ round $ (0 * toRational r + 1 * toRational g + 0 * toRational b)
+			
+toBlue :: (Image i, Convertible i RGB) => i -> RGB
+toBlue img =
+    let grey = convert img :: RGB
+    in fromFunction (shape img) $ \pt ->
+        let pix = grey `index` pt
+            r = fromIntegral $ rgbRed pix
+            g = fromIntegral $ rgbGreen pix
+            b = fromIntegral $ rgbBlue pix
+            tr = 0
+            tg = 0
+            tb = blue r g b
+            pixel = RGBPixel { rgbRed = tr, rgbGreen = tg, rgbBlue = tb } in
+        pixel
+        where
+			blue :: Int -> Int -> Int -> Word8
+			blue r g b = intToWord8 $ round $ (0 * toRational r + 0 * toRational g + 1 * toRational b)
+            
+
+
+toSepia :: (Image i, Convertible i RGB) => i -> RGB
+toSepia img =
+    let grey = convert img :: RGB
+    in fromFunction (shape img) $ \pt ->
+        let pix = grey `index` pt
+            r = fromIntegral $ rgbRed pix
+            g = fromIntegral $ rgbGreen pix
+            b = fromIntegral $ rgbBlue pix
+            tr = sepiaR r g b
+            tg = sepiaG r g b
+            tb = sepiaB r g b
+            pixel = RGBPixel { rgbRed = tr, rgbGreen = tg, rgbBlue = tb } in
+        pixel
+        where
+            sepiaR :: Int -> Int -> Int -> Word8
+            sepiaR r g b = intToWord8 $ round $ min (0.393 * toRational r + 0.769 * toRational g + 0.189 * toRational b) 255
+            sepiaG :: Int -> Int -> Int -> Word8
+            sepiaG r g b = intToWord8 $ round $ min (0.349 * toRational r + 0.686 * toRational g + 0.168 * toRational b) 255
+            sepiaB :: Int -> Int -> Int -> Word8
+            sepiaB r g b = intToWord8 $ round $ min (0.272 * toRational r + 0.534 * toRational g + 0.131 * toRational b) 255
+			
+        
+
+			
+intToWord8 :: Int -> Word8
+intToWord8 i = fromInteger $ fromIntegral i
