3ad2f9b160daeb0bedef398cbdcfdb498292b591
Юрий <razor.zhyk@gmail.com>
2019-04-04 21:35:10 +0300

    Add filters BW, Sepia, R, G, B, Inv

--- src/Home.hs
+++ src/Home.hs
@@ -29,6 +29,8 @@ import Data.List
 import Data.Word8
 import Data.Bits
 
+import ImageFilters
+
 instance RenderMessage App FormMessage where
     renderMessage _ _ = defaultFormMessage
 
@@ -36,70 +38,48 @@ getHomeR :: Handler Html
 getHomeR = defaultLayout $ do
     setTitle "Minimal Multifile"
     [whamlet|
-        <p>
-            <a href=@{AddR 5 7}>HTML addition
-        <p>
-            <a href=@{AddR 5 7}?_accept=application/json>JSON addition
-        <p>
-            <a href=@{AddR 10 7}>Haskell soset
-        <p>
-            <form method=post action=@{FilterR} enctype=multipart/form-data>Picture:
+            <script src=https://code.jquery.com/jquery-3.3.1.min.js>
+            <form id=picForm method=post action=@{FilterR} enctype=multipart/form-data>
+                <p>Picture:
                 <input type=file name=picture>
+                <p>Filter:
+                <p><input type=radio name=filter value=sepia>Sepia
+                <p><input type=radio name=filter value=blackwhite>Black White
+                <p><input type=radio name=filter value=inversia>Inversia
+                <p><input type=radio name=filter value=red>Red
+                <p><input type=radio name=filter value=green>Green
+                <p><input type=radio name=filter value=blue>Blue
+                <br>
                 <input type=submit>
-        
     |]
 
 postFilterR :: Handler Html
 postFilterR = defaultLayout $ do
-    setTitle "Minimal Multifile"
+    setTitle "PhotoFilters"
     imgFileInfo <- runInputPost $ ireq fileField "picture"
-    --bytes <- runResourceT $ fileSource imgFileInfo $$ sinkLbs
-    --liftIO $ write2server bytes "1.jpg"
-    --liftIO $ Prelude.writeFile "1.jpg" Char8.unpack(bytes)
     imgFileName <- return(let fn = fileName imgFileInfo in trace (show fn) fn)
+    selFilt <- runInputPost $ ireq textField "filter"
     liftIO $ fileMove imgFileInfo "tempimg"
-    liftIO $ processImage
+    liftIO $ processImage $ Text.unpack selFilt
     sendFile "image" "tempimg"
 
-processImage :: IO ()
-processImage = do
+processImage :: String -> IO ()
+processImage selFilt = do
     io <- JP.readImage "tempimg"
-    case io of
-        Left err           -> do
-            putStrLn "Unable to load the image:"
-            print err
-        Right (JP.ImageRGB8 img) -> do
-            fridayRGB <- return $ Friday.toFridayRGB img
-            processed <- return $ toSepia fridayRGB
-            JP.savePngImage "tempimg" $ JP.ImageRGB8 $ Friday.toJuicyRGB $ processed
-            putStrLn "Success"
-        Right (JP.ImageYCbCr8 img) -> do
-            fridayRGB <- return $ Friday.toFridayRGB $ JP.convertImage img
-            processed <- return $ toSepia fridayRGB
-            JP.savePngImage "tempimg" $ JP.ImageRGB8 $ Friday.toJuicyRGB $ processed
-            putStrLn "Success"
-
-toSepia :: (Image i, Convertible i RGB) => i -> RGB
-toSepia img =
-    let grey = convert img :: RGB
-    in fromFunction (shape img) $ \pt ->
-        let pix = grey `index` pt
-            r = fromIntegral $ rgbRed pix
-            g = fromIntegral $ rgbGreen pix
-            b = fromIntegral $ rgbBlue pix
-            tr = sepiaR r g b
-            tg = sepiaG r g b
-            tb = sepiaB r g b
-            pixel = RGBPixel { rgbRed = tr, rgbGreen = tg, rgbBlue = tb } in
-        pixel
-        where
-            sepiaR :: Int -> Int -> Int -> Word8
-            sepiaR r g b = intToWord8 $ round $ min (0.393 * toRational r + 0.769 * toRational g + 0.189 * toRational b) 255
-            sepiaG :: Int -> Int -> Int -> Word8
-            sepiaG r g b = intToWord8 $ round $ min (0.349 * toRational r + 0.686 * toRational g + 0.168 * toRational b) 255
-            sepiaB :: Int -> Int -> Int -> Word8
-            sepiaB r g b = intToWord8 $ round $ min (0.272 * toRational r + 0.534 * toRational g + 0.131 * toRational b) 255
-
-
-intToWord8 :: Int -> Word8
-intToWord8 i = fromInteger $ fromIntegral i
+    fridayRGB <- return $
+        case io of
+            Right (JP.ImageRGB8 img) -> do
+                Friday.toFridayRGB img
+            Right (JP.ImageYCbCr8 img) -> do
+                Friday.toFridayRGB $ JP.convertImage img
+    processed <- return $
+        case selFilt of
+            "sepia" -> toSepia fridayRGB
+            "blackwhite" -> toBlackWhite fridayRGB
+            "inversia" -> toInversia fridayRGB
+            "red" -> toRed fridayRGB
+            "green" -> toGreen fridayRGB
+            "blue" -> toBlue fridayRGB
+    --toSepia fridayRGB
+    JP.savePngImage "tempimg" $ JP.ImageRGB8 $ Friday.toJuicyRGB $ processed
+    putStrLn "Success"
--- src/ImageFilters.hs
+++ src/ImageFilters.hs
@@ -79,6 +79,33 @@ toBlue img =
 			blue r g b = intToWord8 $ round $ (0 * toRational r + 0 * toRational g + 1 * toRational b)
             
 
+toBlackWhite :: (Image i, Convertible i RGB) => i -> RGB
+toBlackWhite img =
+    let grey = convert img :: RGB
+    in fromFunction (shape img) $ \pt ->
+        let pix = grey `index` pt
+            r = fromIntegral $ rgbRed pix
+            g = fromIntegral $ rgbGreen pix
+            b = fromIntegral $ rgbBlue pix
+            tr = intToWord8 $ round $ min ((toRational r + toRational g + toRational b)/3) 255
+            tg = intToWord8 $ round $ min ((toRational r + toRational g + toRational b)/3) 255
+            tb = intToWord8 $ round $ min ((toRational r + toRational g + toRational b)/3) 255
+            pixel = RGBPixel { rgbRed = tr, rgbGreen = tg, rgbBlue = tb } in
+        pixel 
+
+toInversia :: (Image i, Convertible i RGB) => i -> RGB
+toInversia img =
+    let grey = convert img :: RGB
+    in fromFunction (shape img) $ \pt ->
+        let pix = grey `index` pt
+            r = fromIntegral $ rgbRed pix
+            g = fromIntegral $ rgbGreen pix
+            b = fromIntegral $ rgbBlue pix
+            tr = intToWord8 $ round $ min (255-toRational r) 255 
+            tg = intToWord8 $ round $ min (255-toRational g) 255 
+            tb = intToWord8 $ round $ min (255-toRational b) 255
+            pixel = RGBPixel { rgbRed = tr, rgbGreen = tg, rgbBlue = tb } in
+        pixel
 
 toSepia :: (Image i, Convertible i RGB) => i -> RGB
 toSepia img =
@@ -88,21 +115,11 @@ toSepia img =
             r = fromIntegral $ rgbRed pix
             g = fromIntegral $ rgbGreen pix
             b = fromIntegral $ rgbBlue pix
-            tr = sepiaR r g b
-            tg = sepiaG r g b
-            tb = sepiaB r g b
+            tr = intToWord8 $ round $ min (0.393 * toRational r + 0.769 * toRational g + 0.189 * toRational b) 255
+            tg = intToWord8 $ round $ min (0.349 * toRational r + 0.686 * toRational g + 0.168 * toRational b) 255
+            tb = intToWord8 $ round $ min (0.272 * toRational r + 0.534 * toRational g + 0.131 * toRational b) 255
             pixel = RGBPixel { rgbRed = tr, rgbGreen = tg, rgbBlue = tb } in
         pixel
-        where
-            sepiaR :: Int -> Int -> Int -> Word8
-            sepiaR r g b = intToWord8 $ round $ min (0.393 * toRational r + 0.769 * toRational g + 0.189 * toRational b) 255
-            sepiaG :: Int -> Int -> Int -> Word8
-            sepiaG r g b = intToWord8 $ round $ min (0.349 * toRational r + 0.686 * toRational g + 0.168 * toRational b) 255
-            sepiaB :: Int -> Int -> Int -> Word8
-            sepiaB r g b = intToWord8 $ round $ min (0.272 * toRational r + 0.534 * toRational g + 0.131 * toRational b) 255
-			
-        
 
-			
 intToWord8 :: Int -> Word8
 intToWord8 i = fromInteger $ fromIntegral i
